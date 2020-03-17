04ae4519cdb4708d8bc3dbf32386b8537f0f193d
ysx <3597107837@qq.com>
2020-03-17 21:40:14 +0800

    Spring多模块+注解配置类+Spring JDBC

---
+++ ioc/src/main/java/com/soft1851/spring/ioc/entity/ConfigManagerTest.java
@@ -0,0 +1,21 @@
+package com.soft1851.spring.ioc.entity;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
+public class ConfigManagerTest {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("demo.xml");
+        ConfigManager configManager = (ConfigManager) context.getBean("configManager");
+        List<String> list = configManager.getList();
+        Map<String, String> map = configManager.getMap();
+        Properties properties = configManager.getProperties();
+        Set<String> set = configManager.getSet();
+        System.out.println("list:"+list+" map:"+map+" properties:"+properties+" set:"+set);
+    }
+}
---
+++ ioc/src/main/java/com/soft1851/spring/ioc/entity/Forum.java
@@ -0,0 +1,15 @@
+package com.soft1851.spring.ioc.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor   //有参
+@NoArgsConstructor    //无参
+@Builder
+public class Forum {
+    private Integer forumId;
+    private String forumName;
+}
